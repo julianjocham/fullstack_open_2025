abf2ae3ac149ebd60760ce144a87621e1e263f7c
julianjocham <julian.jocham@neteleven.de>
2025-09-22 16:34:33 +0200

    middlewares

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,7 +1,19 @@
 const express = require('express')
 
 const app = express()
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
 app.use(express.json())
+
+app.use(requestLogger)
+
 
 let notes = [
     {
@@ -86,6 +98,11 @@ app.patch('/api/notes/:id', (request, response) => {
     }
 })
 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
