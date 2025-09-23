682f4b50c146759be46215546d2aaf1392eb673f
julianjocham <julian.jocham@neteleven.de>
2025-09-23 14:31:16 +0200

    build static files for part3 backend

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -12,7 +12,7 @@ const requestLogger = (request, response, next) => {
 const cors = require('cors')
 
 app.use(cors())
-
+app.use(express.static('dist'))
 app.use(express.json())
 
 app.use(requestLogger)
