bfd364769569c7e38db37c772d72c43cea9c6ecd
julianjocham <julian.jocham@neteleven.de>
2025-09-23 11:37:05 +0200

    part3 cors

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -9,6 +9,9 @@ const requestLogger = (request, response, next) => {
     console.log('---')
     next()
 }
+const cors = require('cors')
+
+app.use(cors())
 
 app.use(express.json())
 
@@ -104,7 +107,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
