476172f9333248725430682158e38f16d857c479
julianjocham <julian.jocham@neteleven.de>
2025-09-23 15:07:58 +0200

    build production-ready static files for phonebook backend

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -102,7 +102,13 @@ app.get('/info', (request, response) => {
     response.send(html)
 })
 
-const PORT = 3001
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
